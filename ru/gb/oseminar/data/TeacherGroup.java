245152fe8106724e692a8afec5904887fe2823a8
Andrey <bot6923777@outlook.com>
2024-03-26 00:37:01 +0300

    добавил

---
+++ ru/gb/oseminar/data/TeacherGroup.java
@@ -0,0 +1,36 @@
+package ru.gb.oseminar.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TeacherGroup {
+    // private List<Teacher> teachers;
+    // String teacher;
+
+    // public TeacherGroup() {
+    // teachers = new ArrayList<>();
+    // }
+
+    // public void setTeacher(List<Teacher> students) {
+    // this.teachers = students;
+    // }
+
+    // public void addTeacherGroup(Teacher teahcer) {
+    // teachers.add(teacher);
+
+    // public Teacher getTeacher() {
+    // return teacher;
+    // }
+
+    // public void setTeacher(Teacher teacher) {
+    // this.teacher = teacher;
+    // }
+
+    // @Override
+    // public String toString() {
+    // return "TeacherGroup{" +
+    // ", teacher=" + teacher +
+    // '}';
+    // }
+
+}
